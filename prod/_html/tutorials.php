7aec505df0f8c223681f222fe13d9fa7d0fa61e9
Ruslan Kuznetsov <donkihot117@gmail.com>
2016-05-15 21:21:22 +0400

    .case-grid--3, .case--tutorial and tutorial.php page

---
+++ prod/_html/tutorials.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="ru">
+<?php
+$pageName = "Обучающие презентации";
+include 'base/head.php';?>
+<body>
+
+<div class="page page-cases">
+    <div class="page-inner">
+        <?php include 'base/header.php';?>
+
+        <div class="page-body">
+            <?php include "components/page-head.php";?>
+
+
+            <div class="cases-catalog">
+                <div class="wrapper">
+                    <div class="cases-grid cases-grid--3">
+
+                        <div class="cases-grid__col">
+                            <div class="case case--tutorial">
+
+                                <a class="case__main-link js-video-popup"
+                                   href="http://www.slideshare.net/slideshow/embed_code/31129952?rel=0">
+                                    <div class="case__img-wr">
+                                        <img src="http://www.mindbox.ru/Content/images/presentations/002.jpg" alt="">
+                                    </div>
+                                </a>
+
+                            </div>
+                        </div>
+
+                        <div class="cases-grid__col">
+                            <div class="case case--tutorial">
+
+                                <a class="case__main-link js-video-popup"
+                                   href="http://www.slideshare.net/slideshow/embed_code/31129952?rel=0">
+                                    <div class="case__img-wr">
+                                        <img src="http://www.mindbox.ru/Content/images/presentations/002.jpg" alt="">
+                                    </div>
+                                </a>
+
+                            </div>
+                        </div>
+
+                        <div class="cases-grid__col">
+                            <div class="case case--tutorial">
+
+                                <a class="case__main-link js-video-popup"
+                                   href="http://www.slideshare.net/slideshow/embed_code/31129952?rel=0">
+                                    <div class="case__img-wr">
+                                        <img src="http://www.mindbox.ru/Content/images/presentations/002.jpg" alt="">
+                                    </div>
+                                </a>
+
+                            </div>
+                        </div>
+
+
+                    </div>
+                </div>
+            </div>
+
+        </div><!-- /page-body -->
+
+        <?php include 'base/footer.php';?>
+
+        <div class="page-inner__overlay"></div>
+    </div><!-- /page-inner -->
+
+    <?php include 'base/popups.php';?>
+
+    <!-- popups -->
+
+</div><!-- /page -->
+
+<?php include 'base/scripts.php'; ?>
+</body>
+</html>
